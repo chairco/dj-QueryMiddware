e34164ace719d4ed2a629185ea7f7794141e686d
yc <yi-chieh.yc.chen@mail.foxconn.com>
2017-10-26 16:36:56 +0800

    Add sql query function

--- src/autosearch/query.py
+++ src/autosearch/query.py
@@ -1,3 +1,4 @@
+# autosearch/qyery.py
 from django.db import connections
 
 
@@ -12,7 +13,9 @@ def dictfetchall(cursor):
 
 
 def get_edc_glass_history(glass_id):
-    """query edc db data
+    """From array_pds_glass_t table
+    type glass_id: str
+    rtype: dict(step_id, glass_id, start_time)
     """
     with connections['eda'].cursor() as cursor:
         cursor.execute(
@@ -27,3 +30,70 @@ def get_edc_glass_history(glass_id):
         )
         queryset = dictfetchall(cursor)
     return queryset
+
+
+def get_edc_data(glass_id, step_id, start_time):
+    """From array_pds_glass_summary_v
+    :type glass_id: str
+    :type step_id: str
+    :type start_time: Datatime()
+    :rtype: dict()
+    """
+    with connections['eda'].cursor() as cursor:
+        cursor.execute(
+            """
+            SELECT *
+            FROM lcdsys.array_pds_glass_summary_v t
+            WHERE 1=1 
+            AND t.GLASS_ID = :glass_id
+            AND t.STEP_ID = :step_id
+            AND t.GLASS_START_TIME = :start_time
+            """,
+            {
+                'glass_id': glass_id,
+                'step_id': step_id,
+                'start_time': start_time
+            }
+        )
+        queryset = dictfetchall(cursor)
+    return queryset
+
+
+def get_teg_glass_history(glass_id):
+    """Here is teg history
+    rtype
+    """
+    pass
+
+
+def get_teg_data(glass_id, step_id, start_time):
+    """Here is teg summary data
+    rtype
+    """
+    pass
+
+
+def get_teg_param_data(glass_id, step_id, start_time):
+    """
+    """
+    pass
+
+
+def get_teg_result(glass_id, step_id, start_time, param_name):
+    """Here is teg raw data with one
+    rtype
+    """
+    pass
+
+
+def get_teg_result_sub(glass_id, step_id, start_time):
+    """
+    """
+    pass
+
+
+def get_sid_with_param(glass_id):
+    """
+    """
+    pass
+
--- src/autosearch/serializers.py
+++ src/autosearch/serializers.py
@@ -2,6 +2,6 @@ from rest_framework import serializers
 
 
 class GlasshisSerializer(serializers.Serializer):
-    GLASS_ID = serializers.CharField(max_length=None, min_length=None, allow_blank=True, required=False)
-    STEP_ID = serializers.CharField(max_length=None, min_length=None, allow_blank=True, required=False)
+    GLASS_ID = serializers.CharField()
+    STEP_ID = serializers.CharField()
     GLASS_START_TIME = serializers.DateTimeField() 
