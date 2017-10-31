a727b5f09e9c1bdd03c6c8504eccf3213d8223df
yc <yi-chieh.yc.chen@mail.foxconn.com>
2017-10-31 16:30:38 +0800

    Add parse to conver timestamp from str to datetime

--- src/autosearch/views.py
+++ src/autosearch/views.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 from django.http import HttpResponse, Http404, JsonResponse
 from django.db.utils import DatabaseError
 
@@ -10,6 +11,8 @@ from rest_framework import mixins
 from autosearch import query, serializers
 
 from datetime import datetime
+
+from dateutil.parser import parse
 
 
 class EdcGlassHistoryList(APIView):
@@ -45,7 +48,11 @@ class EdcSummaryList(APIView):
         glass_id = requests.GET.get('glassid', None)
         step_id = requests.GET.get('stepid', None)
         start_time = requests.GET.get('starttime', None)
-        start_time = datetime.strptime(start_time, "%Y%m%d%H%M%S") # conver to datetime, but oracle also accept format "201712101010"
+        # Convert timestamps with parser.
+        # 注意傳遞過程 2016-11-10T00:54:22+08:00 時區 + 號會被拿掉要在後端先處理過。
+        # https://stackoverflow.com/questions/12281975/convert-timestamps-with-offset-to-datetime-obj-using-strptime
+        # http://labix.org/python-dateutil#head-a23e8ae0a661d77b89dfb3476f85b26f0b30349c
+        start_time = parse(start_time)
         queryset = query.get_edc_data(glass_id, step_id, start_time)
         serializer = serializers.EdcSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -56,6 +63,7 @@ class EdcSummaryList(APIView):
         glass_id = body.get('glassid', None), 
         step_id = body.get('stepid', None)
         start_time = body.get('starttime', None)
+        start_time = parse(start_time)
         queryset = query.get_edc_data(glass_id, step_id, start_time)
         serializer = EdcGlasshisSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
