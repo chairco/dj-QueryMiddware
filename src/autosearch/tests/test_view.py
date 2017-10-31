e56f657a39a62ed1bbe38a0f6511885a7ebe07f7
yc <yi-chieh.yc.chen@mail.foxconn.com>
2017-10-31 16:36:57 +0800

    Update test

--- src/autosearch/tests/test_view.py
+++ src/autosearch/tests/test_view.py
@@ -4,19 +4,6 @@ from django.core.urlresolvers import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 
-from .views import EdcGlasscHistoryViewSet
-
-
-class ViewSetTest(TestCase):
-    """test suite for the api viewset
-    """
-
-    def test_viewset(self):
-        requests = APIRequestFactory().get('/edcgh/')
-        edc_detail = EdcGlasscHistoryViewSet.as_view({'get': 'list'})
-        response = edc_detail(requests)
-        self.assertEqual(response.status_code, 200)
-
 
 class ViewTestCase(TestCase):
     """test suite for the api view
@@ -27,6 +14,10 @@ class ViewTestCase(TestCase):
         """
         self.client = APIClient()
     
-    def test_view(self):
-        response = self.client.get('/fdv/')
+    def test_view_edcs(self):
+        response = self.client.get('/autosearch/edcs')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_edch(self):
+        response = self.client.get('/autosearch/edch')
         self.assertEqual(response.status_code, 200)
