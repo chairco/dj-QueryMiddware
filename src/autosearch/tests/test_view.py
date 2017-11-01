6fa927b03d67b6d4872e75dafa614e5bec708efe
yc <yi-chieh.yc.chen@mail.foxconn.com>
2017-11-01 09:39:10 +0800

    Add teg's test

--- src/autosearch/tests/test_view.py
+++ src/autosearch/tests/test_view.py
@@ -6,18 +6,26 @@ from rest_framework import status
 
 
 class ViewTestCase(TestCase):
-    """test suite for the api view
+    """test suite for the APIview
     """
 
     def setup(self):
         """Define the test client and other test variable
         """
         self.client = APIClient()
-    
+
     def test_view_edcs(self):
         response = self.client.get('/autosearch/edcs')
         self.assertEqual(response.status_code, 200)
 
     def test_view_edch(self):
         response = self.client.get('/autosearch/edch')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_tegs(self):
+        response = self.client.get('/autosearch/tegs')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_tegh(self):
+        response = self.client.get('/autosearch/tegh')
         self.assertEqual(response.status_code, 200)
