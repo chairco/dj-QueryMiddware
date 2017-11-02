d095729cfa7096b029ffa7e13c760a96eb31d286
yc <yi-chieh.yc.chen@mail.foxconn.com>
2017-11-02 09:38:25 +0800

    Update example

--- example_r.R
+++ example_r.R
@@ -2,7 +2,23 @@
 # client should install.packages('curl') 
 # packages fromJSON is default install after R 3.3.3 
 
-# get one glassid from rest
-baseurl <- "http://localhost:8000/autosearch/edch/?glassid="
-res <- fromJSON(paste0(baseurl, "TL6AS0KAF"), flatten=True)
-stepid <- res$STEP_ID
+
+library(jsonlite)
+library(curl)
+
+baseurl <- 'http://localhost:8000/autosearch/edch/?glassid='
+glassid <- read.csv(file='sample-10.csv', header=FALSE)$V1
+glassid <- as.character(glassid)
+datas <- list()
+
+# Sequence to get the glass data.
+for (gid in glassid) {
+  url <- paste0(baseurl, gid)
+  res <- fromJSON(url)
+  datas[[gid]] <- c(res)
+  # Outut csv file by glassid.
+  write.csv(res, paste0(gid, '.csv'))
+}
+
+
+
